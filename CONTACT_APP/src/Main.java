df3cf5e6d8b9bd30979fecdbb329740c48e04dc4
om shankar <om8003golu@gmail.com>
2025-04-30 17:08:56 +0530

    switch case added to contact app

--- CONTACT_APP/src/Main.java
+++ CONTACT_APP/src/Main.java
@@ -1,19 +1,74 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 
+        Scanner sc = new Scanner(System.in);
+        int choice;
         ContactManager cm = new ContactManager();
-        cm.addContact("Ravi Kumar","4465646456","rkravi@xyz.com");
-        cm.addContact("Suman Sankar Kumar","4365646456","sus@xyz.com");
-        cm.addContact("Mohan Bhagwat","9965646456","bhagwatMohan@xyz.com");
-        cm.addContact("shivam dubey","8765646456","dubey@xyz.com");
-        cm.addContact("Ravi Phogat","8565646456","raphogat@xyz.com");
+        do{
+            System.out.println("...Enter a valid option and hit enter...");
+            System.out.println("Enter 1 to view all contacts");
+            System.out.println("Enter 2 to add new contact");
+            System.out.println("Enter 3 to delete a contact");
+            System.out.println("Enter 4 to search a  contact");
+            System.out.println("Enter 5 to Exit program.");
 
-//        cm.showAllContacts();
-//        cm.removeContactByName("ravi Kumar");
-//        cm.showAllContacts();
 
-//        cm.searchContactByName("mohan bhagwa");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice){
+                case 1:{
+                    cm.showAllContacts();
+                    break;
+                }
+
+                case 2:{
+                    System.out.println("Enter name: ");
+                    String name = sc.nextLine();
+                    System.out.println("Enter phoneNo: ");
+                    String phoneNo = sc.nextLine();
+                    System.out.println("Enter email: ");
+                    String email = sc.nextLine();
+                    cm.addContact(name,phoneNo,email);
+                    break;
+                }
+
+
+            case 3:{
+                System.out.println("Enter name: ");
+                String name = sc.nextLine();
+                cm.removeContactByName(name);
+                break;
+
+            }
+            case 4:{
+                System.out.println("Enter name: ");
+                String name = sc.nextLine();
+                cm.searchContactByName(name);
+                break;
+
+            }
+                case 5:{
+                    System.out.println("Thankyou for chosing our app.");
+                    break;
+                }
+                default:
+                    System.out.println("Invalid input");
+
+
+            }
+
+
+
+        }while(choice != 5);
+
+        sc.close();
+
+
 
     }
 }
